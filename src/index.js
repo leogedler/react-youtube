88c65f2da8e732e70fcb5ed098c671188f2dcb62
leonardogedler <leogedler@hotmail.com>
2017-01-16 13:29:34 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyCgG6titLIo55t544kPJgPnSAWo7V1W3wA';
+
+const App = () => {
+    return <div>Hi</div>;
+}
+
+
+// Render elements to the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
