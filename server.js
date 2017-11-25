4e4a8d0d778a454120231cf7e55525e2a2fe8f1e
leonardogedler <leogedler@hotmail.com>
2017-11-25 15:40:41 -0400

    changed server.js

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ module.exports = {
     const indexPath = path.join(__dirname, '/index.html')
     const publicPath = express.static(path.join(__dirname, '../public'))
 
-    app.use('/public', publicPath)
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
+    // app.use('/public', publicPath)
+    // app.get('/', function (_, res) { res.sendFile(indexPath) })
+    
+    // Rewrite rule
+    app.use(express.static(__dirname + '/public'));
+    app.get('/public', (req, res)=>{
+        res.sendFile(__dirname + '/public');
+    });
+    app.get('/', (req, res)=>{
+        res.sendFile(__dirname + '/index.html');
+    });
 
     return app
   }
