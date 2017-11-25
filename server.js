757cd8b06e42a68e2d413678c17c3f6bf555f133
leonardogedler <leogedler@hotmail.com>
2017-11-25 15:45:46 -0400

    changed index bundle path

--- server.js
+++ server.js
@@ -7,17 +7,8 @@ module.exports = {
     const indexPath = path.join(__dirname, '/index.html')
     const publicPath = express.static(path.join(__dirname, '../public'))
 
-    // app.use('/public', publicPath)
-    // app.get('/', function (_, res) { res.sendFile(indexPath) })
-    
-    // Rewrite rule
-    app.use(express.static(__dirname + '/public'));
-    app.get('/public', (req, res)=>{
-        res.sendFile(__dirname + '/public');
-    });
-    app.get('/', (req, res)=>{
-        res.sendFile(__dirname + '/index.html');
-    });
+    app.use('/public', publicPath)
+    app.get('/', function (_, res) { res.sendFile(indexPath) })
 
     return app
   }
