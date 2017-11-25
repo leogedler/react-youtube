32490fc78908f539d6790d4596f3963b30ad7c2f
leonardogedler <leogedler@hotmail.com>
2017-11-25 17:21:59 -0400

    Changed webpack config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -8,7 +8,7 @@ module.exports = {
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, '/public'),
     filename: 'bundle.js',
     publicPath: '/public/'
   },
